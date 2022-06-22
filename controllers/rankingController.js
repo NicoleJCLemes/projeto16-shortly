b605abf3f8f1c45baad5aba78042ded7cf9f3526
Rodrigodcs <rodrigodcsee@gmail.com>
2022-06-22 10:18:58 -0300

    Preparando revisão de código

--- controllers/rankingController.js
+++
@@ -1,5 +0,0 @@
-import database from "../database.js";
-
-export async function getRanking (req, res) {
-
-}
--- schemas/urlsSchemas.js
+++
@@ -1,7 +0,0 @@
-import joi from "joi";
-
-const shortUrlSchema = joi.object({
-    url: joi.pattern(/^http:\/\/|https:\/\//)
-});
-
-export default shortUrlSchema
